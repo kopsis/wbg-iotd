002edc00a79ad01ff8a0948786e9b7a2e965c821
David J. Kessler <dkessler@acm.org>
2026-04-03 10:44:25 -0700

    Added overlays.default to flake.

--- flake.nix
+++ flake.nix
@@ -32,21 +32,11 @@
         tllist
         wayland
         wayland-protocols
-        # Tipp: you can use `nix-locate foo.h` to find the package that provides a header file, see https://github.com/nix-community/nix-index
       ];
       nativeBuildInputs = with pkgs; [
         # add build dependencies here
-        ## For mesonbuild:
         meson ninja
-        ## For cmake:
-        #cmake
-        ## For autotools:
-        # autoconf-archive
-        # autoreconfHook
         pkg-config
-        # clangd language server.
-        # Also start your IDE/editor from the shell provided by `nix develop` as the wrapped clangd from clang-tools needs environment variables set by the shell
-        #clang-tools
         wayland-scanner
       ];
     in
@@ -77,5 +67,16 @@
           install -m755 -D ./wbg-iotd $out/bin/wbg-iotd
         '';
       };
+
+      overlays.default = final: prev: {
+        wbg-iotd = final.stdenv.mkDerivation {
+          inherit buildInputs nativeBuildInputs pname version src;
+          NIX_CFLAGS_COMPILE = "-O1 -DWBG_VERSION=\"${version}\"";
+          installPhase = ''
+            install -m755 -D ./wbg-iotd $out/bin/wbg-iotd
+          '';
+        };
+      };
+
     });
 }
