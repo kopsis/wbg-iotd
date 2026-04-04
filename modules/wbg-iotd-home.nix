52ea06d1fc8221f37ccd3e1152f98952ace11672
David J. Kessler <dkessler@acm.org>
2026-04-04 05:57:32 -0700

    Removed 'config' from top level of settings.

--- modules/wbg-iotd-home.nix
+++ modules/wbg-iotd-home.nix
@@ -20,23 +20,21 @@ in
     };
   };
 
-  config = lib.mkIf cfg.services.wbg-iotd.enable {
-    systemd.user.services = {
-      wbg-iotd = {
-        Unit = {
-          Description = "Wallpaper image-of-the-day manager.";
-          PartOf = [ "graphical-session.target" ];
-          After = [ "graphical-session.target" ];
-        };
-        Service = {
-          Type = "exec";
-          ExecStart = "${wbg-iotd-bin}";
-          Restart = "always";
-          RestartSec = 10;
-        };
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
-        };
+  systemd.user.services = lib.mkIf cfg.services.wbg-iotd.enable {
+    wbg-iotd = {
+      Unit = {
+        Description = "Wallpaper image-of-the-day manager.";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+      };
+      Service = {
+        Type = "exec";
+        ExecStart = "${wbg-iotd-bin}";
+        Restart = "always";
+        RestartSec = 10;
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
       };
     };
   };
